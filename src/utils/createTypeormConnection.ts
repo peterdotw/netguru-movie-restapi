7f4167124e13bf7033f281baf76ce5645b2a10a5
peterdotw <piotrwiderapraca@gmail.com>
2021-02-25 15:57:56 +0100

    final fix 2

--- src/utils/createTypeormConnection.ts
+++ src/utils/createTypeormConnection.ts
@@ -4,9 +4,9 @@ export const createTypeormConnection = async (): Promise<Connection> => {
   const connectionOptions = await getConnectionOptions(
     process.env.IS_DOCKER ? "docker-db" : "default"
   );
-  console.log(connectionOptions);
 
   return createConnection({
     ...connectionOptions,
+    name: "default",
   });
 };
