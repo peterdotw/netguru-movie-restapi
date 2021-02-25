f5115aaed93f4b5b18f3c7ede2f32a5a7d74021f
peterdotw <piotrwiderapraca@gmail.com>
2021-02-25 15:24:33 +0100

    postgres

--- src/utils/createTypeormConnection.ts
+++ src/utils/createTypeormConnection.ts
@@ -1,10 +1,5 @@
-import { getConnectionOptions, createConnection } from "typeorm";
+import { createConnection } from "typeorm";
 
 export const createTypeormConnection = async () => {
-  const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
-  console.log(connectionOptions);
-  return createConnection({
-    ...connectionOptions,
-    name: process.env.IS_DOCKER ? "docker-db" : "default",
-  });
+  return createConnection("docker-db");
 };
