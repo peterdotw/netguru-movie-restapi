a06d646ed555d050bdcde75b23946d0e922b578a
peterdotw <piotrwiderapraca@gmail.com>
2021-02-25 14:48:08 +0100

    docker-compose fix

--- src/utils/createTypeormConnection.ts
+++ src/utils/createTypeormConnection.ts
@@ -2,6 +2,7 @@ import { getConnectionOptions, createConnection } from "typeorm";
 
 export const createTypeormConnection = async () => {
   const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
+  console.log(process.env.IS_DOCKER);
   return createConnection({
     ...connectionOptions,
     name: process.env.IS_DOCKER ? "docker-db" : "default",
